af54f4054a940763f24379f3131aeb1bbe0086c3
bebold-jhr <100561882+bebold-jhr@users.noreply.github.com>
2026-03-21 13:44:14 +0100

    test: data/aws_auditmanager*

---
+++ integration-tests/data/aws_auditmanager_control/main.tf
@@ -0,0 +1,4 @@
+data "aws_auditmanager_control" "this" {
+  name = "1. Risk Management"
+  type = "Standard"
+}
---
+++ integration-tests/modules/data/aws_auditmanager_control/main.tf
@@ -0,0 +1,3 @@
+resource "aws_auditmanager_account_registration" "this" {
+  deregister_on_destroy = true
+}
